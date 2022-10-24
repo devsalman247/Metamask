1bf6ed9c190567b2aa4884c4a62300b4ef14387b
devsalman247 <salman247.dev@gmail.com>
2022-10-24 14:18:19 +0500

    project init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className='flex flex-col items-center'>
+      <button className='px-3 py-2 rounded bg-slate-500 text-white'>Connect to Metamask</button>
+    </div>
+  )
+}
+
+export default App
